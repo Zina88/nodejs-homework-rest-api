f5bb1d4f75f7eca84744e1a4a4adee330d9b130c
Zina <zinasaenko@hotmail.com>
2022-12-13 10:25:00 +0200

    fix

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,7 +3,6 @@ const { HttpError } = require("../helpers");
 const validateBody = (schema, message) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
-    console.log(schema.validate(req.body));
 
     if (error) {
       next(HttpError(400, message));
